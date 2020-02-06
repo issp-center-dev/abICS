91b93692254ebbf24f77bf73cff5cc8e87ad412d
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2020-02-06 14:44:31 +0900

    add test for subprocess mode in openmx

--- test/openmx/test_openmx_io.py
+++ test/openmx/test_openmx_io.py
@@ -10,13 +10,11 @@ if __name__ == '__main__':
     solver = OpenMXSolver(path_to_solver)
     print ("solver name is {}. ".format(solver.name()))
 
-
     base_dir = "/Users/k-yoshimi/Dropbox/PycharmProjects/abICS/test/openmx/sample"
     #Read base file
     print("Read base file.")
     input = solver.input
     input.from_directory(base_dir)
-
     #Execute OpenMX
     print("Write input file.")
     input_dir = "test"
@@ -37,6 +35,7 @@ if __name__ == '__main__':
     print("Execute OpenMX.")
     cmd = "openmx {}.dat".format(os.path.join(input_dir, input.base_openmx_input["System.Name"][0]))
     subprocess.call(cmd.split())
+    # Check the results of test/met.dat# and test1/met.dat# are almost same.
 
     #Check seldyn_arr
     phys.structure.add_site_property("seldyn", [[False, False, False]] * len(phys.structure.sites))
@@ -47,3 +46,17 @@ if __name__ == '__main__':
     print("Execute OpenMX.")
     cmd = "openmx {}.dat".format(os.path.join(input_dir, input.base_openmx_input["System.Name"][0]))
     subprocess.call(cmd.split())
+    # Check the coordinates of test1/met.dat# and test2/met.dat# are same.
+
+    # test subprocess mode
+    from abics.applications.latgas_abinitio_interface.run_base_mpi import runner
+    from mpi4py import MPI
+    nprocs_per_replica = 1
+    energy_calculator = runner(
+        base_input_dir=base_dir,
+        Solver=solver,
+        nprocs_per_solver=nprocs_per_replica,
+        comm=MPI.COMM_SELF,
+        solver_run_scheme = "subprocess"
+    )
+    energy_calculator.submit(structure=phys.structure, output_dir="./")
