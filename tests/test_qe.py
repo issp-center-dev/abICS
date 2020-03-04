e5cf9846ded781df17ad9dcbae03fff703d9e86e
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2020-03-04 16:28:20 +0900

    update unittest for qe

--- tests/test_qe.py
+++ tests/test_qe.py
@@ -7,32 +7,42 @@ from pymatgen import Structure
 
 from abics.applications.latgas_abinitio_interface.qe import QESolver
 
+from qe_tools.parsers import PwInputFile
+
 
 class TestQE(unittest.TestCase):
     def setUp(self):
         self.solver = QESolver(".")
-        self.datadir = os.path.join(os.path.dirname(__file__), "data", "qe")
+        self.rootdir = os.path.dirname(__file__)
+        self.datadir = os.path.join(self.rootdir, "data", "qe")
+        self.workdir = os.path.join(self.rootdir, "res", "qe")
 
     def test_get_results(self):
-        res = self.solver.output.get_results(os.path.join(self.datadir, "ref"))
-        ref = Structure.from_file(os.path.join(self.datadir, "..", "ref_pos.vasp"))
-        res.structure.to("POSCAR", os.path.join(self.datadir, "res_pos.vasp"))
+        res = self.solver.output.get_results(self.datadir)
+        ref = Structure.from_file(os.path.join(self.datadir, "pos.vasp"))
+        res.structure.to("POSCAR", os.path.join(self.workdir, "pos.vasp"))
         ref_energy = -1039.9018832347706
         self.assertTrue(res.structure.matches(ref))
         self.assertTrue(np.isclose(res.energy, ref_energy))
 
     def test_input(self):
         self.solver.input.from_directory(os.path.join(self.datadir, "baseinput"))
-        A = np.ones((3, 3)) - np.eye(3)
-        r = np.array([0.0, 0.1, 0.2])
-        st = Structure(A, ["Al"], [r], coords_are_cartesian=False)
+        A = 4.0*np.eye(3)
+        r = np.array([[0.0, 0.0, 0.0], [0.5, 0.5, 0.5]])
+        st = Structure(A, ["Al", "Al"], r, coords_are_cartesian=False)
 
         self.solver.input.update_info_by_structure(st)
-        self.assertTrue(np.allclose(self.solver.input.pwi.cell_parameters["cell"], A))
-        self.assertTrue(
-            np.allclose(self.solver.input.pwi.atomic_positions["positions"], r)
-        )
-        self.solver.input.write_input(os.path.join(self.datadir, "work"))
+        self.solver.input.write_input(self.workdir)
+        res = PwInputFile(os.path.join(self.workdir, "scf.in"))
+        ref = PwInputFile(os.path.join(self.datadir, "scf.in"))
+
+        res.namelists["CONTROL"]["pseudo_dir"] = ref.namelists["CONTROL"]["pseudo_dir"]
+        res.namelists["CONTROL"]["outdir"] = ref.namelists["CONTROL"]["outdir"]
+
+        self.assertEqual(res.namelists, ref.namelists)
+
+        self.assertTrue(np.allclose(res.cell_parameters["cell"], ref.cell_parameters["cell"]))
+        self.assertTrue(np.allclose(res.atomic_positions["positions"], ref.atomic_positions["positions"]))
 
     def test_cl_algs(self):
         nprocs = 2
