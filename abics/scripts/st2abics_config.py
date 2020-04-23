d117ac6093bf0f90f8605ba73ceb13b5a14c543c
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2020-04-23 10:40:53 +0900

    format with black

--- abics/scripts/st2abics_config.py
+++ abics/scripts/st2abics_config.py
@@ -8,21 +8,23 @@ def main():
 
     # Input parser
     parser = argparse.ArgumentParser(
-        description='Prepare abICS config from structure file')
+        description="Prepare abICS config from structure file"
+    )
 
-    parser.add_argument('inputfi',
-                        help='toml input file for %(prog)s',
+    parser.add_argument("inputfi", help="toml input file for %(prog)s")
+    parser.add_argument(
+        "structurefi",
+        help="Structure file that can be read"
+        + " by pymatgen Structure.from_file() method",
     )
-    parser.add_argument('structurefi',
-                        help='Structure file that can be read' + \
-                        ' by pymatgen Structure.from_file() method',
+    parser.add_argument(
+        "outfi",
+        nargs="?",
+        type=argparse.FileType("w"),
+        default=sys.stdout,
+        help="Output file to be used as abics input." + " Defaults to standard output",
     )
-    parser.add_argument('outfi', nargs='?', type=argparse.FileType('w'),
-                        default=sys.stdout,
-                        help='Output file to be used as abics input.' + \
-                        ' Defaults to standard output',
-    )
-                        
+
     args = parser.parse_args()
     abics_input_dict = {}
 
@@ -32,85 +34,85 @@ def main():
     input_species = list(input_structure.symbol_set)
 
     # Make supercell
-    input_structure.make_supercell(st2abics_params['supercell'])
+    input_structure.make_supercell(st2abics_params["supercell"])
 
     # Make base structure
-    base_params = st2abics_params['base_structure']
-    base_species = base_params['species']
-    fix = base_params['fix']
+    base_params = st2abics_params["base_structure"]
+    base_species = base_params["species"]
+    fix = base_params["fix"]
 
     base_structure = input_structure.copy()
     remove_species = [sp for sp in input_species if sp not in base_species]
     base_structure.remove_species(remove_species)
-    abics_input_dict['config'] = {
-        'unitcell': base_structure.lattice.matrix.tolist(),
-        'supercell': [1,1,1],
+    abics_input_dict["config"] = {
+        "unitcell": base_structure.lattice.matrix.tolist(),
+        "supercell": [1, 1, 1],
     }
     if len(base_structure.symbol_set) == 0:
-        abics_input_dict['config']['base_structure'] = [{}]
+        abics_input_dict["config"]["base_structure"] = [{}]
     else:
-        abics_input_dict['config']['base_structure'] = []
+        abics_input_dict["config"]["base_structure"] = []
     for sp in base_structure.symbol_set:
         if fix:
-            relaxations = [[False, False, False]]*int(base_structure.composition[sp])
+            relaxations = [[False, False, False]] * int(base_structure.composition[sp])
         else:
-            relaxations = [[True, True, True]]*int(base_structure.composition[sp])
-        abics_input_dict['config']['base_structure'].append(
+            relaxations = [[True, True, True]] * int(base_structure.composition[sp])
+        abics_input_dict["config"]["base_structure"].append(
             {
-            'type': sp,
-            'coords': base_structure.frac_coords[
-                base_structure.indices_from_symbol(sp),:
+                "type": sp,
+                "coords": base_structure.frac_coords[
+                    base_structure.indices_from_symbol(sp), :
                 ].tolist(),
-            'relaxation': relaxations
+                "relaxation": relaxations,
             }
         )
 
     # Make defect structures
-    abics_input_dict['config']['defect_structure'] = []
-    defect_params = st2abics_params['defect_structure']
+    abics_input_dict["config"]["defect_structure"] = []
+    defect_params = st2abics_params["defect_structure"]
     for defect_sublattice in defect_params:
-        site_center_species = defect_sublattice['site_center_species']
+        site_center_species = defect_sublattice["site_center_species"]
         defect_structure = input_structure.copy()
-        remove_species = [
-            sp for sp in input_species if sp not in site_center_species
-            ]
+        remove_species = [sp for sp in input_species if sp not in site_center_species]
         defect_structure.remove_species(remove_species)
         coords = defect_structure.frac_coords.tolist()
         groups = []
-        if 'groups' in defect_sublattice.keys():
-            for group in defect_sublattice['groups']:
+        if "groups" in defect_sublattice.keys():
+            for group in defect_sublattice["groups"]:
                 groups.append(
-                    {'name': group['name'],
-                     'species': group.get('species', [group['name']]),
-                     'coords': group.get('coords', [[[0,0,0]]]),
-                     'num': int(group['num']),
-                     }
+                    {
+                        "name": group["name"],
+                        "species": group.get("species", [group["name"]]),
+                        "coords": group.get("coords", [[[0, 0, 0]]]),
+                        "num": int(group["num"]),
+                    }
                 )
         else:
             for sp in site_center_species:
                 groups.append(
-                    {'name': sp,
-                     'species': [sp],
-                     'coords': [[[0,0,0]]],
-                     'num': int(defect_structure.composition[sp]),
-                     }
+                    {
+                        "name": sp,
+                        "species": [sp],
+                        "coords": [[[0, 0, 0]]],
+                        "num": int(defect_structure.composition[sp]),
+                    }
                 )
         num = 0
         for group in groups:
-            num += group['num']
-        assert(num == len(coords))
+            num += group["num"]
+        assert num == len(coords)
 
-        abics_input_dict['config']['defect_structure'].append(
-            {'coords': coords,
-             'groups': groups,
-             }
+        abics_input_dict["config"]["defect_structure"].append(
+            {"coords": coords, "groups": groups}
         )
 
     config = toml.dumps(abics_input_dict)
-    with open(os.path.join(os.path.dirname(__file__), 'input_template.toml'), 'r') as fi:
+    with open(
+        os.path.join(os.path.dirname(__file__), "input_template.toml"), "r"
+    ) as fi:
         template = fi.read()
     args.outfi.write(template + config)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-    
