de703dae346894e4d212b0ee8feb9814f66f59b5
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2020-09-03 17:02:13 +0900

    cleanup

--- abics/applications/latgas_abinitio_interface/aenet_trainer.py
+++ abics/applications/latgas_abinitio_interface/aenet_trainer.py
@@ -1,10 +1,19 @@
 from abics.applications.latgas_abinitio_interface import aenet
 import numpy as np
-import os, pathlib, shutil,subprocess
+import os, pathlib, shutil, subprocess
+
 
 class aenet_trainer:
-    def __init__(self, structures, energies, generate_inputdir, train_inputdir, predict_inputdir, 
-                    generate_exe, train_exe):
+    def __init__(
+        self,
+        structures,
+        energies,
+        generate_inputdir,
+        train_inputdir,
+        predict_inputdir,
+        generate_exe,
+        train_exe,
+    ):
         self.structures = structures
         self.energies = energies
         self.generate_inputdir = generate_inputdir
@@ -17,14 +26,16 @@ class aenet_trainer:
         self.is_prepared = False
         self.is_trained = False
         self.generate_outputdir = None
-        
+
     def prepare(self):
         # prepare XSF files for aenet
         os.makedirs("aenetXSF", exist_ok=True)
         os.chdir("aenetXSF")
-        for i,st in enumerate(self.structures):
+        for i, st in enumerate(self.structures):
             xsf_string = aenet.to_XSF(st, write_force_zero=True)
-            xsf_string = "# total energy = {} eV\n\n".format(self.energies[i]) + xsf_string
+            xsf_string = (
+                "# total energy = {} eV\n\n".format(self.energies[i]) + xsf_string
+            )
             with open("structure.{}.xsf".format(i), "w") as fi:
                 fi.write(xsf_string)
         xsfdir = os.getcwd()
@@ -36,16 +47,27 @@ class aenet_trainer:
         os.chdir("generate")
         with open("generate.in.head", "r") as fi:
             generate_head = fi.read()
-            xsf_paths = [os.path.join(xsfdir, "structure.{}.xsf".format(i)) for i in range(self.numdata)]
-            generate = generate_head + \
-                "\n" + "FILES\n" + str(self.numdata) + "\n" + \
-                "\n".join(xsf_paths) + "\n"
+            xsf_paths = [
+                os.path.join(xsfdir, "structure.{}.xsf".format(i))
+                for i in range(self.numdata)
+            ]
+            generate = (
+                generate_head
+                + "\n"
+                + "FILES\n"
+                + str(self.numdata)
+                + "\n"
+                + "\n".join(xsf_paths)
+                + "\n"
+            )
             with open("generate.in", "w") as fi_in:
                 fi_in.write(generate)
-        
+
         command = self.generate_exe + " generate.in"
         with open(os.path.join(os.getcwd(), "stdout"), "w") as fi:
-            subprocess.run(command, shell=True, stdout=fi, stderr=subprocess.STDOUT, check=True)
+            subprocess.run(
+                command, shell=True, stdout=fi, stderr=subprocess.STDOUT, check=True
+            )
         self.generate_outputdir = os.getcwd()
         os.chdir(pathlib.Path(os.getcwd()).parent)
         self.is_prepared = True
@@ -54,19 +76,23 @@ class aenet_trainer:
         try:
             assert self.is_prepared
         except AssertionError as e:
-            e.args += ('you have to prepare the trainer before training!')
+            e.args += "you have to prepare the trainer before training!"
         if os.path.exists("train"):
             shutil.rmtree("train")
         shutil.copytree(self.train_inputdir, "train")
         os.chdir("train")
-        shutil.copyfile(os.path.join(self.generate_outputdir, "aenet.train"), 
-                                    os.path.join(os.getcwd(), "aenet.train"))
+        shutil.copyfile(
+            os.path.join(self.generate_outputdir, "aenet.train"),
+            os.path.join(os.getcwd(), "aenet.train"),
+        )
         command = self.train_exe + " train.in"
-        
+
         while True:
             # Repeat until test set error begins to rise
             with open(os.path.join(os.getcwd(), "stdout"), "w") as fi:
-                subprocess.run(command, shell=True, stdout=fi, stderr=subprocess.STDOUT, check=True)
+                subprocess.run(
+                    command, shell=True, stdout=fi, stderr=subprocess.STDOUT, check=True
+                )
             with open("stdout", "r") as trainout:
                 fullout = trainout.readlines()
                 epoch_data = []
@@ -74,14 +100,14 @@ class aenet_trainer:
                     if "<" in li:
                         epoch_data.append(li)
             with open("epochdat", "w") as epochdatfi:
-                epochdat_str = "".join(epoch_data[1:]).replace("<","")
+                epochdat_str = "".join(epoch_data[1:]).replace("<", "")
                 epochdatfi.write(epochdat_str)
             epoch_dat_arr = np.loadtxt("epochdat")
             # Find epoch id with minimum test set RMSE
-            testRMSE = epoch_dat_arr[:,4]
+            testRMSE = epoch_dat_arr[:, 4]
             minID = np.argmin(testRMSE)
             num_epoch = len(testRMSE)
-            if minID < num_epoch - 10: # this "10" is a heuristic
+            if minID < num_epoch - 10:  # this "10" is a heuristic
                 break
 
         print("Best fit at epoch ID ", minID)
@@ -94,19 +120,16 @@ class aenet_trainer:
         try:
             assert self.is_trained
         except AssertionError as e:
-            e.args += ('you have to train before getting results!')
+            e.args += "you have to train before getting results!"
         os.makedirs(baseinput_dir, exist_ok=True)
-        shutil.copyfile(os.path.join(self.predict_inputdir, "predict.in"), 
-                        os.path.join(baseinput_dir, "predict.in"))
+        shutil.copyfile(
+            os.path.join(self.predict_inputdir, "predict.in"),
+            os.path.join(baseinput_dir, "predict.in"),
+        )
         NNPid_str = "{:05d}".format(self.train_minID)
-        NNPfiles = [fi for fi in os.listdir(self.train_outputdir) 
-                        if NNPid_str in fi]
+        NNPfiles = [fi for fi in os.listdir(self.train_outputdir) if NNPid_str in fi]
         for fi in NNPfiles:
-            shutil.copyfile(os.path.join(self.train_outputdir, fi),
-                            os.path.join(baseinput_dir, fi[:-6]))
-        
-
-        
-
-
-            
+            shutil.copyfile(
+                os.path.join(self.train_outputdir, fi),
+                os.path.join(baseinput_dir, fi[:-6]),
+            )
--- misc/MC2DFT/train.py
+++ misc/MC2DFT/train.py
@@ -1,6 +1,4 @@
-from abics.mc_mpi import (
-    RefParams
-)
+from abics.mc_mpi import RefParams
 from abics.applications.latgas_abinitio_interface.params import DFTParams, TrainerParams
 from abics.applications.latgas_abinitio_interface import aenet_trainer
 from abics.applications.latgas_abinitio_interface import map2perflat
@@ -12,6 +10,7 @@ from abics.applications.latgas_abinitio_interface.defect import (
 from pymatgen import Structure
 import numpy as np
 import os, sys
+
 
 def main_impl(tomlfile):
 
@@ -44,18 +43,19 @@ def main_impl(tomlfile):
     structures = []
     energies = []
 
+    # val_map is a list of list [[sp0, vac0], [sp1, vac1], ...]
     if vac_map:
-        vac_map = {sp0: sp1 for sp0, sp1 in vac_map}
+        vac_map = {specie: vacancy for specie, vacancy in vac_map}
     else:
         vac_map = {}
 
     for dir in ALdirs:
         for rpl in range(nreplicas):
             os.chdir(os.path.join(dir, str(rpl)))
-            energies_ref = np.loadtxt("energy_corr.dat")[:,1]
+            energies_ref = np.loadtxt("energy_corr.dat")[:, 1]
             for i, energy in enumerate(energies_ref):
                 structure = Structure.from_file("structure_rel.{}.vasp".format(i))
-                # map to perfect lattice 
+                # map to perfect lattice
                 structure = map2perflat(perf_st, structure, vac_map)
                 if ignore_species:
                     structure.remove_species(ignore_species)
@@ -74,11 +74,19 @@ def main_impl(tomlfile):
 
     generate_exe = trainer_commands[0]
     train_exe = trainer_commands[1]
-    trainer = aenet_trainer(structures, energies, generate_inputdir, train_inputdir, predict_inputdir, 
-                generate_exe, train_exe)
+    trainer = aenet_trainer(
+        structures,
+        energies,
+        generate_inputdir,
+        train_inputdir,
+        predict_inputdir,
+        generate_exe,
+        train_exe,
+    )
     trainer.prepare()
     trainer.train()
     trainer.new_baseinput(base_input_dir[0])
+
 
 if __name__ == "__main__":
     tomlfile = sys.argv[1] if len(sys.argv) > 1 else "input.toml"
