73ee62d7f70636d3072efb429949e0a683549021
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2021-07-21 16:59:34 +0900

    import Lattice from pymatgen.core

--- abics/applications/latgas_abinitio_interface/defect.py
+++ abics/applications/latgas_abinitio_interface/defect.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see http://www.gnu.org/licenses/.
 
-from pymatgen import Lattice
+from pymatgen.core import Lattice
 
 from .model_setup import config, defect_sublattice, base_structure
 
